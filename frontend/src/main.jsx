bd943ab6ecfc4d29a2148a847ef2c7e905bd2dad
Atheek <atheek.mohomed@outlook.com>
2024-06-23 00:32:31 +0530

    env file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 console.log(import.meta.env.VITE_NODE_ENV);
 console.log(process.env.VITE_BACKEND_URL, "urlss");
-console.log(import.meta.VITE_BACKEND_URL, "metaurl");
+console.log(import.meta.env.VITE_BACKEND_URL, "metaurl");
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
