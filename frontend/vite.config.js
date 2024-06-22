025dbcd1a271fda2e5a06acf8bb724630b86dc20
Atheek <atheek.mohomed@outlook.com>
2024-06-23 02:02:10 +0530

    new solution

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,22 +2,23 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const API_URL = env.VITE_BACKEND_URL;
 
-  return defineConfig({
+  return {
     server: {
       proxy: {
         "/api": {
-          target: process.env.VITE_BACKEND_URL,
+          target: API_URL,
           changeOrigin: true,
           secure: false,
         },
       },
     },
     plugins: [react()],
-  });
-};
+  };
+});
 // export default defineConfig({
 //   plugins: [react()],
 //   server: {
