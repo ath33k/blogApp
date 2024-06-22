091038e099e0e693ab47bf51e8dff103cdf192cd
Atheek <atheek.mohomed@outlook.com>
2024-06-22 23:23:35 +0530

    revert changes

--- backend/app.js
+++ backend/app.js
@@ -13,31 +13,13 @@ const app = express();
 // app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
 const corsOptions = {
-  origin: `${process.env.FRONTEND_URL}`,
+  origin: process.env.FRONTEND_URL,
   optionsSuccessStatus: 200,
   credentials: true,
 };
 
-// app.options("*", cors(corsOptions));
-// app.use(cors(corsOptions));
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
-
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-  }
-  next();
-});
+app.options("*", cors(corsOptions));
+app.use(cors(corsOptions));
 // app.options("*", function (req, res) {
 //   res.status(200);
 // });
