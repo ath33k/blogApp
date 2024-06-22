1d75a2fbe4dbbc56565b9ee989822e49696c2ecb
Atheek <atheek.mohomed@outlook.com>
2024-06-22 18:10:47 +0530

    made middleware changes

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 // ROUTES
 //mounting routes
-app.use("/", (req, res, next) => {
+app.get("/", (req, res, next) => {
   res.status(200).json({
     status: "Success",
     message: "hellow from server",
