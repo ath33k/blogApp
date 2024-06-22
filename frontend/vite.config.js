a6b596251e89c7152183a1e45ab58b0e72d831e2
Atheek <atheek.mohomed@outlook.com>
2024-06-23 00:37:40 +0530

    env typo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+  const env = loadEnv(mode, process.cwd(), "");
 
   return defineConfig({
     plugins: [react()],
