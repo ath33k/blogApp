cb7b16b74cb1375dbde56609fc67c2401e15b830
Atheek <atheek.mohomed@outlook.com>
2024-06-23 01:18:28 +0530

    chechling the loadenv

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-  console.log(env);
+
   return defineConfig({
     plugins: [react()],
 
     define: {
-      "process.env.VITE_BACKEND_URL": JSON.stringify(
-        process.env.VITE_BACKEND_URL
-      ),
+      "process.env.VITE_BACKEND_URL": JSON.stringify(env),
     },
     server: {
       proxy: {
