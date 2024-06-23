999dbb011a902ff26548ed2b7754c983fc9b4e07
Atheek <atheek.mohomed@outlook.com>
2024-06-23 15:53:14 +0530

    created previous config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
     plugins: [react()],
     server: {
       proxy: {
         "/api": {
-          target: "https://blog-app-backend-phi.vercel.app",
+          target: env.VITE_BACKEND_URL,
           changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
         },
       },
     },
