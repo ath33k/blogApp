2f434de1d6e076a79f22c03f30ba61e39300a458
Atheek <atheek.mohomed@outlook.com>
2024-06-23 15:28:39 +0530

    rewrite vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,19 +4,18 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-  const API_URL = env.VITE_BACKEND_URL;
 
   return {
+    plugins: [react()],
     server: {
       proxy: {
         "/api": {
-          target: API_URL,
+          target: env.VITE_BACKEND_URL,
           changeOrigin: true,
-          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
         },
       },
     },
-    plugins: [react()],
   };
 });
 // export default defineConfig({
